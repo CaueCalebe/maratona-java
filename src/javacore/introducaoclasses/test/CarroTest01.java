67449d99df180fc82ff76212d1f2b8bdc08697e9
CaueCalebe <cauecalebe070@gmail.com>
2026-07-25 17:58:49 -0300

    refactor: atualizar sintaxe CarroTest01.java

--- src/javacore/introducaoclasses/test/CarroTest01.java
+++ src/javacore/introducaoclasses/test/CarroTest01.java
@@ -6,10 +6,18 @@ import javacore.introducaoclasses.dominio.Carro;
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+        Carro carro2 = new Carro();
+        
         carro.nome = "Fiat Argo";
-        carro.modelo = "Hatche";
+        carro.modelo = "Hatch";
         carro.ano = 2023;
         
+        carro2.nome = "Hyundai HB20";
+        carro2.modelo = "Hatch";
+        carro2.ano = 2016;
+        
         System.out.println("Nome: " + carro.nome + "\nModelo: " + carro.modelo + "\nAno de Fabricação: " + carro.ano);
+        System.out.println("-------------------------------");
+        System.out.println("Nome: " + carro2.nome + "\nModelo: " + carro2.modelo + "\nAno de Fabricação: " + carro2.ano);
     }
 }
