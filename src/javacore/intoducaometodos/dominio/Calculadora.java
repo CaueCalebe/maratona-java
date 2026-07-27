6d20d8e8efc1bb4de5ad75878920cd463b6f092c
CaueCalebe <cauecalebe070@gmail.com>
2026-07-27 20:24:37 -0300

    refactor: atualizar sintaxe Calculadora.java

--- src/javacore/intoducaometodos/dominio/Calculadora.java
+++ src/javacore/intoducaometodos/dominio/Calculadora.java
@@ -9,4 +9,12 @@ public class Calculadora {
     public void subtraiDoisNumeros() {
         System.out.println(21 - 2);
     }
+    
+    public void multiplicacaoDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+    
+    public double divideDoisNumeros(double num1, double num2) {
+        return 0;
+    }
 }
