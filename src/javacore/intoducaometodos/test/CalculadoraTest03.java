9d054d9a5ce3c08c9d128f045f3216a00157a85b
CaueCalebe <cauecalebe070@gmail.com>
2026-07-27 20:23:28 -0300

    feat: adicionar CalculadoraTest03.java

---
+++ src/javacore/intoducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package javacore.intoducaometodos.test;
+
+import javacore.intoducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(20, 2);
+        System.out.println(result);
+    }
+}
