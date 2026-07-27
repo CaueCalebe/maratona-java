64e5ba0840d48a04d5852097d8dcfd55c03bb910
CaueCalebe <cauecalebe070@gmail.com>
2026-07-27 20:23:01 -0300

    feat: adicionar CalculadoraTest02.java

---
+++ src/javacore/intoducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javacore.intoducaometodos.test;
+
+import javacore.intoducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicacaoDoisNumeros(10, 20);
+    }
+}
