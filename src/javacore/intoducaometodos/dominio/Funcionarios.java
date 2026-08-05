aeaac5ec575d5be9ca6b800dd57e90a74fa077ca
CaueCalebe <cauecalebe070@gmail.com>
2026-08-05 19:00:41 -0300

    feat: adicionar Funcionarios.java

---
+++ src/javacore/intoducaometodos/dominio/Funcionarios.java
@@ -0,0 +1,33 @@
+package javacore.intoducaometodos.dominio;
+
+public class Funcionarios {
+    
+    public String nome;
+    public int idade;
+    public double[] salarios;
+    
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        if (salarios != null)
+        for (double salario: salarios) {
+            System.out.println(salario + " ");
+        }
+    }
+    
+    public void calcularMediaSalarios() {
+        
+        if (salarios == null) {
+            return;
+        }
+        
+        double media = 0;
+        
+        for (double salario: salarios) {
+            media += salario;
+        }
+        
+        media /= salarios.length;
+        System.out.println("Média Salarial: " + media);
+    }
+}
